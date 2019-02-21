b65658cfd825fa3c8e88e6abd6cf2d168547f701
Gordon Moseley <moseleygj@gmail.com>
2019-02-20 23:13:30 -0500

    ijs started

---
+++ FortuneCookie/JS/generateInfo.js
@@ -0,0 +1,14 @@
+/*Another Fortune Button*/
+
+
+/*let fortune = document.getElementById("fortuneText");*/
+
+function newFortune(){
+let numberz = document.querySelector("#luckyNumbers");
+let fortune = document.querySelector("#fortuneTextContent");
+  console.log("Nothing here yet...");
+
+  numberz.innerHTML="1,5,2,6,19,5";
+ /* fortune.innerHTML="1,45,2,6,19,5";*/
+
+}
