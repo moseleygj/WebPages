30f28da51f79535011a1c42f1dd9e3a74350bff4
Gordon Moseley <moseleygj@gmail.com>
2019-02-22 22:48:00 -0500

    rAJAX and JSON added

--- FortuneCookie/JS/generateInfo.js
+++ FortuneCookie/JS/generateInfo.js
@@ -3,17 +3,53 @@ let numberz = document.querySelector("#luckyNumbers");
 let fortune = document.querySelector("#fortuneTextContent");
 let randomNumArray=[];
 function newFortune(){
-     randomNumArray.length=0; //empty the array
-     console.log("hi. from the random number function!");
- 
-    for (num = 0;num < 6; num++){
-    randomNumArray.push(Math.floor(Math.random()*49)+1);
+     randomNumArray.length=0; //EMPTY ARRAY
+      
+    for (num = 0; num < 6; num++){
+        randomNumArray.push(Math.floor(Math.random() * 49) + 1);
     }
-    numberz.innerHTML="";
-        for (i = 0; i < randomNumArray.length; i++){
-        numberz.innerHTML += randomNumArray[i]+",";
+
+    numberz.innerHTML = "";
+
+    for (i = 0; i < randomNumArray.length; i++){
+        numberz.innerHTML += randomNumArray[i] + ",";
     }
-    //find and remove the last comma
-    numberz.innerHTML = numberz.innerHTML.slice(0,-1);
-        console.log(numberz.innerHTML);
+      numberz.innerHTML = numberz.innerHTML.slice(0, -1);  //REMOVE LAST COMMA
+      loadJSON();
+}
+
+function loadJSON(){
+var data_file = "fortuneCookie.json";
+var http_request = new XMLHttpRequest();
+try{
+    
+    http_request = new XMLHttpRequest();    //opera, FireFox, Chrome, Safari
+}
+catch (e){
+    //Internet Explorer Browsers
+    try{
+        http_request = new ActiveXObject("Msxml2.XMLHTTP");
+    }
+    catch (e) {
+        try{
+            http_request = new ActiveXObject("Microsoft.XMLHTTP");
+        }catch (e){
+            //something went wrong
+            alert("Your Browser Broke!");
+            return false;
+        }
+    }
+}
+http_request.onreadystatechange = function(){
+    if (http_request.readyState == 4){
+        var jsonObj = JSON.parse(http_request.responseText);    //JS function JSON.parse to parse JSON data
+        //jsonObj varibale now contains the data structre and can
+        //be accessed as jsonObj.name and jsonObj.country.
+       let rand=Math.floor(Math.random()*254)+1;
+        document.getElementById("fortuneTextContent").innerHTML = jsonObj[rand];
+    }
+}
+
+http_request.open("GET", data_file, true);
+http_request.send();
 }
