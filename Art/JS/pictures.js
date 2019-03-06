81b8f2f7631850f230464a582195c5d693a68752
Gordon Moseley <moseleygj@gmail.com>
2019-03-05 21:40:34 -0500

    background blur of 'container' set

--- Art/JS/pictures.js
+++ Art/JS/pictures.js
@@ -4,13 +4,15 @@ $(document).ready(function(){
     $(".hold").css("display","flex");
     //$(this).css("filter","blur(0px)").fadeTo(3500);
     $("#bigPic").fadeIn(1200);
+    
     var me = $(this).attr('src');
     $("#showTime").attr("src",me);
     });
     //reverse the above
     $(".hold").click(function(){
      $("#container").css("filter","blur(0px)");
-    $(".hold").css("display","none");
+    //$(".hold").css("display","none");
+    $(".hold").fadeOut(600);
     $(this).css("filter","blur(0px)").fadeOut(1500);
     $("#bigPic").fadeOut(1200);
     var me = $(this).attr('src');
