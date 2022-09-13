88cbfa8717b5f9cc1141873fdbd293b9c60fdee7
moseleygj <moseleygj@gmail.com>
2022-09-12 22:27:25 -0400

    section addition

--- LandingPageResume/script2.js
+++ LandingPageResume/script2.js
@@ -1,50 +1,73 @@
 function skills() {
-  alert("skills");
-  document.getElementById("skills").style.borderBottom = "1px solid orange";
-  document.getElementById("skills").style.color = "red";
-  document.getElementById("bottomLinks").style.display = "none";
-}
-function exp() {
-    document.getElementById("myStory0").style.display = "none";
+    alert("skills");
+    document.getElementById("skills").style.borderBottom = "1px solid orange";
+    document.getElementById("skills").style.color = "red";
+    document.getElementById("middlePortion").style.height = "auto";
 
-  document.getElementById("myStory1").style.display = "block";
-  document.getElementById("myStory2").style.display = "none";
-  document.getElementById("myStory3").style.display = "none";
-  document.getElementById("myStory").style.display = "none";
+    document.getElementById("myStory1").style.display = "none";
+    /* document.getElementById("bottomLinks").style.display = "none";*/
+}
+
+function exp() {
+    /*document.getElementById("myStory0").style.display = "none";
+    document.getElementById("myStory1").style.display = "block";
+    document.getElementById("myStory2").style.display = "none";
+    document.getElementById("myStory3").style.display = "none";
+    document.getElementById("myStory").style.display = "none";*/
+    document.getElementById("myStory0").style.opacity = "0";
+    document.getElementById("myStory1").style.opacity = "1";
+    document.getElementById("myStory1").style.display = "block";
+    document.getElementById("middlePortion").style.height = "3200px";
+    document.getElementById("myStory2").style.opacity = "0";
+    document.getElementById("myStory3").style.opacity = "0";
+    document.getElementById("myStory").style.opacity = "0";
 }
 
 function edu() {
-  document.getElementById("myStory1").style.display = "none";
-  document.getElementById("myStory3").style.display = "none";
+    /*
+    document.getElementById("myStory1").style.display = "none";
+    document.getElementById("myStory3").style.display = "none";
     document.getElementById("myStory4").style.display = "none";
-
-  document.getElementById("myStory0").style.display = "none";
-  document.getElementById("myStory").style.display = "none";
-  document.getElementById("myStory2").style.display = "block";
+    document.getElementById("myStory0").style.display = "none";
+    document.getElementById("myStory").style.display = "none";
+    document.getElementById("myStory2").style.display = "block";*/
+    document.getElementById("myStory0").style.opacity = "0";
+    document.getElementById("myStory1").style.opacity = "0";
+    document.getElementById("myStory2").style.opacity = "1";
+    document.getElementById("myStory3").style.opacity = "0";
+    document.getElementById("myStory").style.opacity = "0";
+    document.getElementById("middlePortion").style.height = "auto";
+    document.getElementById("myStory1").style.display = "none";
 }
 
 function skl() {
-    document.getElementById("myStory0").style.display = "none";
-
-  document.getElementById("myStory1").style.display = "none";
-  document.getElementById("myStory2").style.display = "none";
-  document.getElementById("myStory3").style.display = "block";
+    document.getElementById("middlePortion").style.height = "auto";
+    document.getElementById("myStory1").style.display = "none";
+    /*  
+  document.getElementById("myStory0").style.display = "none";
+    document.getElementById("myStory1").style.display = "none";
+    document.getElementById("myStory2").style.display = "none";
+    document.getElementById("myStory3").style.display = "block";
     document.getElementById("myStory4").style.display = "none";
-
-
+*/
+    document.getElementById("myStory0").style.opacity = "0";
+    document.getElementById("myStory1").style.opacity = "0";
+    document.getElementById("myStory2").style.opacity = "0";
+    document.getElementById("myStory3").style.opacity = "1";
+    document.getElementById("myStory").style.opacity = "0";
 
 }
-function prt(){
 
+function prt() {
+    alert("portfolio link will be here!");
 }
 
 function hme() {
-  document.getElementById("myStory1").style.display = "none";
-  document.getElementById("myStory2").style.display = "none";
-  document.getElementById("myStory0").style.display = "block";
-  document.getElementById("myStory").style.display = "none";
-  document.getElementById("myStory3").style.display = "none";
-  document.getElementById("myStory4").style.display = "none";
-
+    document.getElementById("myStory").style.opacity = "0";
+    document.getElementById("myStory0").style.opacity = "1";
+    document.getElementById("myStory1").style.opacity = "0";
+    document.getElementById("myStory2").style.opacity = "0";
+    document.getElementById("myStory3").style.opacity = "0";
+    document.getElementById("hmeLink").style.textDecoration = "underline";
 
 }
