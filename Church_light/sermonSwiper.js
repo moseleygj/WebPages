68c9c70b190c3c8c92947308bf59df891ea495ea
moseleygj <moseleygj@gmail.com>
2022-12-13 14:39:46 -0500

    css & JS updates

--- Church_light/sermonSwiper.js
+++ Church_light/sermonSwiper.js
@@ -17,3 +17,50 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev"
     }
 });
+
+
+function showStuff(a, b) {
+    /* alert(a)*/
+
+    let content = document.getElementById(a);
+    let contentArrow = document.getElementById(b);
+
+    if (content.style.display === "none") {
+        content.style.display = "block";
+
+
+    } else {
+        content.style.display = "none";
+        /* document.getElementById("ar").style.transform="rotate(0deg)";*/
+
+
+
+    }
+}
+
+/*
+     if(document.getElementById(a).style.display==="flex"){
+      alert("else: "+document.getElementById('otherServices').style.display);
+   document.getElementById("boxHold").className="testFadeOut";
+     
+       return 0;
+     }
+     else{
+      alert("ELSE: "+document.getElementById('otherServices').style.display);
+      document.getElementById(b).style.transform="rotate(90deg)"; /*document.getElementById(a).style.display="flex";
+      document.getElementById(a).className="testFadeIn";
+       */
+
+
+/*show more or less videos button*/
+function moreLess() {
+    if (document.getElementById("showMore").style.display == "none") {
+        document.getElementById("showMore").style.display = "flex"
+        document.getElementById("showMoreLessBtn").innerText = "Show Less";
+
+    } else {
+        document.getElementById("showMoreLessBtn").innerText = "Show More";
+        document.getElementById("showMore").style.display = "none";
+    }
+
+}
