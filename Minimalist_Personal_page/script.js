7ce2d63c9be8fb485aa29b57cd628f3d0e42cc23
moseleygj <moseleygj@gmail.com>
2025-03-20 17:12:23 -0400

    syntax error fix

--- Minimalist_Personal_page/script.js
+++ Minimalist_Personal_page/script.js
@@ -1,23 +1,24 @@
-function switchCSS()
-{
-
+function switchCSS() {
+    // Add functionality here if needed.
 }
 
 let now = new Date();
 let hour = now.getHours();
 let minute = now.getMinutes();
+let newMinutes = minute < 10 ? '0' + minute : minute;
 
-console.log(hour + ":" + minute);let formattedTime = now.toLocaleTimeString();
-function whatsTheTime(){
-document.getElementById("timeNow").innerHTML=hour+":"+minute+" - EST";
+console.log(hour + ":" + newMinutes);
+
+let formattedTime = now.toLocaleTimeString();
+
+function whatsTheTime() {
+    document.getElementById("timeNow").innerHTML = hour + ":" + newMinutes + " - EST";
 }
 
-function changeImg()
-{
-    document.getElementById("avatarMain").src="avatarWink.png";
+function changeImg() {
+    document.getElementById("avatarMain").src = "avatarWink.png";
 }
 
-function revert(){
-    document.getElementById("avatarMain").src="avatarDefault.png";
-
+function revert() {
+    document.getElementById("avatarMain").src = "avatarDefault.png";
 }
