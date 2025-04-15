799e09ee7c1629d6e1688e52fe3ee6accd3045bf
moseleygj <moseleygj@gmail.com>
2025-04-15 16:41:30 -0400

    toggle switch upgrade

--- Minimalist_Personal_page/script.js
+++ Minimalist_Personal_page/script.js
@@ -12,8 +12,24 @@ console.log(hour + ":" + newMinutes);
 let formattedTime = now.toLocaleTimeString();
 
 function whatsTheTime() {
-    document.getElementById("timeNow").innerHTML = hour + ":" + newMinutes + " - EST";
+   document.getElementById("timeNow").innerHTML = hour + ":" + newMinutes + " - EST";
+ 
 }
+
+function changeMyStyle(){
+    alert("I've been summoned")
+    const linkElement = document.querySelector('#theme-stylesheet');
+    
+    const currentHref = linkElement.href.split('/').pop();
+
+    if (currentHref === 'style.css') {
+      linkElement.href = 'style1.css';
+    } else {
+      linkElement.href = 'style.css';
+    }
+  }
+
+
 // fix and compress later
 function changeImg() {
     document.getElementById("avatarMain").src = "avatarWink.png";
