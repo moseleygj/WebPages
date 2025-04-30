1209b76932d4123b59f58234f308cab64d637272
moseleygj <moseleygj@gmail.com>
2025-04-29 21:20:38 -0400

    major updates rev.2

--- Minimalist_Personal_page/script.js
+++ Minimalist_Personal_page/script.js
@@ -42,3 +42,26 @@ function changeImg1() {
 function revert() {
     document.getElementById("avatarMain").src = "avatarDefault.png";
 }
+
+
+//toggle for MenuBarTop
+  function toggleMenu() {
+    document.getElementById('menu').classList.toggle('active');
+  }
+
+  //show hash for resume section
+  /*function showHash() {
+    const hash = 'SHA256(PDF): b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9';
+    const hashElement = document.getElementById('hash-value');
+    hashElement.textContent = hash;
+    hashElement.hidden = false;
+  }
+  */
+  function showHash() {
+    const hashPart1 = 'SHA256(PDF): b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7';
+    const hashPart2 = 'SHA256(DOC): b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7';
+    const hashElement = document.getElementById('hash-value');
+    
+    hashElement.innerHTML = `${hashPart1}<br>${hashPart2}`;  // Adding <br> to break the line
+    hashElement.hidden = false;
+  }
