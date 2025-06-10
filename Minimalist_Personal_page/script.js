09e7543899e2bbdfb6a1f199dbdeb48568d116e5
moseleygj <moseleygj@gmail.com>
2025-06-09 23:57:56 -0400

    update cyberSecurity section tags added. CSS update

--- Minimalist_Personal_page/script.js
+++ Minimalist_Personal_page/script.js
@@ -1,7 +1,7 @@
 
   const facts = [
     " I enjoy reverse-engineering systems just to see how they tick—even if they weren’t broken.",
-    " I’ve worked from hotel rooms, client sites, and server closets—always with my ThinkPad and USB toolkit.",
+    " Thinkpad Laptops are known to have some of the best keyboards in the world",
     " I believe an IBM Thinkpad's keyboard is superior to most.",
     " I trust my home server more than most cloud services—and yes, I have a backup for my backup.",
     " If something plugs in, I’ve probably tried to optimize or mod it.",
